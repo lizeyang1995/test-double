16b704b716d0d71b77aac9e1991ebe7c4f3dcfbe
lizeyang1995 <424011527@qq.com>
2020-09-15 23:50:33 +0800

    [lizeyang] feat: complete test use Mockito in GradeService

--- src/test/java/mockito/MockitoGradeServiceTest.java
+++ src/test/java/mockito/MockitoGradeServiceTest.java
@@ -1,4 +1,25 @@
 package mockito;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import stub.GradeService;
+import stub.GradeSystem;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.mockito.Mockito.*;
+
 public class MockitoGradeServiceTest {
+
+    GradeService gradeService;
+
+    @Test
+    public void shouldUseMockito() {
+        GradeSystem stubGradeSystem = Mockito.mock(GradeSystem.class);
+        gradeService = new GradeService(stubGradeSystem);
+        when(stubGradeSystem.gradesFor(123L)).thenReturn(Collections.singletonList(90.0));
+        Assertions.assertEquals(90, gradeService.calculateAverageGrades(123L));
+    }
 }
