99bac278ab81d48bd6ea39721fb2947bdd3bf3dd
lizeyang1995 <424011527@qq.com>
2020-09-15 23:20:02 +0800

    [lizeyang] feat: complete test use Mockito in SecurityCenter

---
+++ src/test/java/mockito/MockitoSecurityCenterTest.java
@@ -0,0 +1,19 @@
+package mockito;
+
+import mock.DoorPanel;
+import mock.SecurityCenter;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class MockitoSecurityCenterTest {
+
+    SecurityCenter securityCenter;
+
+    @Test
+    public void shouldUseMockito() {
+        DoorPanel mockDoorPanel = Mockito.mock(DoorPanel.class);
+        securityCenter = new SecurityCenter(mockDoorPanel);
+        securityCenter.switchOn();
+        Mockito.verify(mockDoorPanel, Mockito.times(1));
+    }
+}
